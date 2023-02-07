8734651937b6aa7d8bbffa38af48079abdace2d0
belsart <graveonthemoon@gmail.com>
2023-02-07 18:40:15 +0300

    circeya

--- src/data/images.js
+++ src/data/images.js
@@ -3,7 +3,7 @@ const images = [];
 (function getImages() {
     for (let i = 1; i <= 4; i++) {
         images.push({
-            image: `./src/assets/images/image${i}.jpg`,
+            image: `/public/images/image${i}.jpg`,
             id: `${i}`,
         });
     }
