97155b9a15179e12ad7eaae554944f8f97e7fd00
belsart <graveonthemoon@gmail.com>
2023-02-07 19:00:57 +0300

    circeya

--- src/data/images.js
+++ src/data/images.js
@@ -1,12 +1,22 @@
-const images = [];
+import image1 from '/src/assets/images/image1.jpg';
+import image2 from '/src/assets/images/image2.jpg';
+import image3 from '/src/assets/images/image3.jpg';
+import image4 from '/src/assets/images/image4.jpg';
 
-(function getImages() {
-    for (let i = 1; i <= 4; i++) {
-        images.push({
-            image: `/public/images/image${i}.jpg`,
-            id: `${i}`,
-        });
-    }
-})();
+const images = [
+    { image: image1, id: 1 },
+    { image: image2, id: 2 },
+    { image: image3, id: 3 },
+    { image: image4, id: 4 },
+];
+
+// (function getImages() {
+//     for (let i = 1; i <= 4; i++) {
+//         images.push({
+//             image: `/public/images/image${i}.jpg`,
+//             id: `${i}`,
+//         });
+//     }
+// })();
 
 export default images;
--- src/data/slides.js
+++ src/data/slides.js
@@ -1,12 +1,32 @@
-const slides = [];
+import slide1 from '/src/assets/slider/slide1.jpg';
+import slide2 from '/src/assets/slider/slide2.jpg';
+import slide3 from '/src/assets/slider/slide3.jpg';
+import slide4 from '/src/assets/slider/slide4.jpg';
+import slide5 from '/src/assets/slider/slide5.jpg';
+import slide6 from '/src/assets/slider/slide6.jpg';
+import slide7 from '/src/assets/slider/slide7.jpg';
+import slide8 from '/src/assets/slider/slide8.jpg';
 
-(function getSlides() {
-    for (let i = 1; i <= 8; i++) {
-        slides.push({
-            image: `/public/slider/slide${i}.jpg`,
-            id: `${i}`,
-        });
-    }
-})();
+const slides = [
+    { image: slide1, id: 1 },
+    { image: slide2, id: 2 },
+    { image: slide3, id: 3 },
+    { image: slide4, id: 4 },
+    { image: slide5, id: 5 },
+    { image: slide6, id: 6 },
+    { image: slide7, id: 7 },
+    { image: slide8, id: 8 },
+];
+
+console.log(slides[0]);
+
+// (function getSlides() {
+//     for (let i = 1; i <= 8; i++) {
+//         slides.push({
+//             image: `/public/slider/slide${i}.jpg`,
+//             id: `${i}`,
+//         });
+//     }
+// })();
 
 export default slides;
