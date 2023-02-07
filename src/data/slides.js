fe7e574c7134467e72bbeb31d262d20b01cbc29b
belsart <graveonthemoon@gmail.com>
2023-02-07 18:25:36 +0300

    Починил ссылки

--- src/data/slides.js
+++ src/data/slides.js
@@ -3,7 +3,7 @@ const slides = [];
 (function getSlides() {
     for (let i = 1; i <= 8; i++) {
         slides.push({
-            image: `/src/assets/slider/slide${i}.jpg`,
+            image: `./src/assets/slider/slide${i}.jpg`,
             id: `${i}`,
         });
     }
